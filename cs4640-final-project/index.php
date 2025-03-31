0039e1eb86bbd1ce276a08d4e458671865ce146f
jocelynjiang1 <jocelynjiang1@gmail.com>
2025-03-30 23:55:54 -0400

    folder to copy into web/www/ for local testing

---
+++ cs4640-final-project/index.php
@@ -0,0 +1,30 @@
+<?php
+// DEBUGGING ONLY! Show all errors.
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+// Class autoloading by name.  All our classes will be in a directory
+// that Apache does not serve publicly.  They will be in /opt/src/, which
+// is our src/ directory in Docker.
+spl_autoload_register(function ($classname) {
+        include "/opt/src/cs4640-final-project/$classname.php";
+});
+
+// CS4640 server
+// public files: public_html
+// private files: anything OUTSIDE of public_html
+//     create a "private" next to public_html
+//     include "/students/mst3k/students/mst3k/private"
+//
+//     if I created "private/triviagame"
+//     include "/students/mst3k/students/mst3k/private/triviagame/$classname.php";:
+
+
+// Other global things that we need to do
+
+// Instantiate the front controller
+$hobbytracker = new HobbyController($_GET); //give the controller data from query string
+
+// Run the controller
+$hobbytracker->run();
+
